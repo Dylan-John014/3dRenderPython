d20e2c2458a6aeedaaf88b0df222be8e81d04637
Dylan John <dylansejohn@gmail.com>
2023-05-28 13:18:56 +0100

    Everything is working
    Feel free to change the obj if you try this out - you will need to tweak a few values and maybe turn down the movement speed in camera.py but this is good enough for now.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from projection import *
 import pygame as pg
 
 
-class SoftwareRenderer:
+class SoftwareRender:
     def __init__(self):
         pg.init()
         self.RES = self.WIDTH, self.HEIGHT = 1600, 900
@@ -15,11 +15,21 @@ class SoftwareRenderer:
         self.create_objects()
 
     def create_objects(self):
-        self.camera = Camera(self, [0.5, 1, -4])
+        self.camera = Camera(self, [-5, 6, -55])
         self.projection = Projection(self)
-        self.object = Object3D(self)
-        self.object.translate([0.2, 0.4, 0.2])
-        self.object.rotate_y(math.pi / 6)
+        self.object = self.get_object_from_file('resources/tank.obj')
+        self.object.rotate_y(-math.pi / 4)
+
+    def get_object_from_file(self, filename):
+        vertex, faces = [], []
+        with open(filename) as f:
+            for line in f:
+                if line.startswith('v '):
+                    vertex.append([float(i) for i in line.split()[1:]] + [1])
+                elif line.startswith('f'):
+                    faces_ = line.split()[1:]
+                    faces.append([int(face_.split('/')[0]) - 1 for face_ in faces_])
+        return Object3D(self, vertex, faces)
 
     def draw(self):
         self.screen.fill(pg.Color('darkslategray'))
@@ -28,15 +38,13 @@ class SoftwareRenderer:
     def run(self):
         while True:
             self.draw()
+            self.camera.control()
             [exit() for i in pg.event.get() if i.type == pg.QUIT]
-            pg.display.set_caption(
-                'Pygame 3D Object Renderer (Numpy Matrices)  |  fps: ' +
-                str(int(self.clock.get_fps()))
-            )
+            pg.display.set_caption(str(self.clock.get_fps()))
             pg.display.flip()
             self.clock.tick(self.FPS)
 
 
 if __name__ == '__main__':
-    app = SoftwareRenderer()
+    app = SoftwareRender()
     app.run()
--- object_3d.py
+++ object_3d.py
@@ -1,31 +1,32 @@
 import pygame as pg
 from matrix_functions import *
+from numba import njit
+
+
+@njit(fastmath=True)
+def any_func(arr, a, b):
+    return np.any((arr == a) | (arr == b))
 
 
 class Object3D:
-    def __init__(self, render):
+    def __init__(self, render, vertices='', faces=''):
         self.render = render
-        self.vertices = np.array([
-            (0, 0, 0, 1),
-            (0, 1, 0, 1),
-            (1, 1, 0, 1),
-            (1, 0, 0, 1),
-            (0, 0, 1, 1),
-            (0, 1, 1, 1),
-            (1, 1, 1, 1),
-            (1, 0, 1, 1)
-        ])
-        self.faces = np.array([
-            (0, 1, 2, 3),
-            (4, 5, 6, 7),
-            (0, 4, 5, 1),
-            (2, 3, 7, 6),
-            (1, 2, 6, 5),
-            (0, 3, 7, 4)
-        ])
+        self.vertices = np.array([np.array(v) for v in vertices])
+        self.faces = np.array([np.array(face) for face in faces])
+        self.translate([0.0001, 0.0001, 0.0001])
+
+        self.font = pg.font.SysFont('Arial', 30, bold=True)
+        self.color_faces = [(pg.Color('orange'), face) for face in self.faces]
+        self.movement_flag, self.draw_vertices = True, True
+        self.label = ''
 
     def draw(self):
         self.screen_projection()
+        self.movement()
+
+    def movement(self):
+        if self.movement_flag:
+            self.rotate_y(-(pg.time.get_ticks() % 0.005))
 
     def screen_projection(self):
         vertices = self.vertices @ self.render.camera.camera_matrix()
@@ -35,14 +36,19 @@ class Object3D:
         vertices = vertices @ self.render.projection.to_screen_matrix
         vertices = vertices[:, :2]
 
-        for face in self.faces:
+        for index, color_face in enumerate(self.color_faces):
+            color, face = color_face
             polygon = vertices[face]
-            if not np.any((polygon == self.render.H_WIDTH) | (polygon == self.render.H_HEIGHT)):
-                pg.draw.polygon(self.render.screen, pg.Color('orange'), polygon, 3)
+            if not any_func(polygon, self.render.H_WIDTH, self.render.H_HEIGHT):
+                pg.draw.polygon(self.render.screen, color, polygon, 1)
+                if self.label:
+                    text = self.font.render(self.label[index], True, pg.Color('white'))
+                    self.render.screen.blit(text, polygon[-1])
 
-        for vertex in vertices:
-            if not np.any((vertex == self.render.H_WIDTH) | (vertex == self.render.H_HEIGHT)):
-                pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 6)
+        if self.draw_vertices:
+            for vertex in vertices:
+                if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
+                    pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 2)
 
     def translate(self, pos):
         self.vertices = self.vertices @ translate(pos)
@@ -58,3 +64,14 @@ class Object3D:
 
     def rotate_z(self, angle):
         self.vertices = self.vertices @ rotate_z(angle)
+
+
+class Axes(Object3D):
+    def __init__(self, render):
+        super().__init__(render)
+        self.vertices = np.array([(0, 0, 0, 1), (1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
+        self.faces = np.array([(0, 1), (0, 2), (0, 3)])
+        self.colors = [pg.Color('red'), pg.Color('green'), pg.Color('blue')]
+        self.color_faces = [(color, face) for color, face in zip(self.colors, self.faces)]
+        self.draw_vertices = False
+        self.label = 'XYZ'
